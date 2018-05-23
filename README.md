3952ddb87f5522c9ca4f86de5d7ec664b0885fb4
Jane Jeon <sungil.ahn.19@dartmouth.edu>
2018-05-23 03:40:06 -0400

    added error checking and removed asking for password every time sudo access was needed

--- README.md
+++ README.md
@@ -1,19 +1,15 @@
 # Fix Shitty Wifi (for macOS)
 Fixes various troublesome wifi connections that show any of the following behaviour: 
-- disconnect randomly (just bad connection)
+- disconnect randomly (spotty/intermittently dropping connection)
 - silently cuts you off from the rest of the internet even though the wifi is still connected (*cough* comcast *cough*)
 - forces you to sign in every X minutes (think airport wifi)
 
-## Example Usage
-`node index.js` to start, `.env` for configuration.
+## Sample Usage
+`sudo node index.js` to start, `.env` for configuration.
 
 <p align='center'>
-  <img src='screenshot1.png' width='70%'>
+  <img src='screenshot.png' width='70%'>
 </p>
 
-### A word about administrator access
-To fix troublesome behaviour #3, the program needs to spoof the computer's `MAC address`. To do that, it needs `sudo` access. Thus, it will ask you for your password (popup shown below) every time it is necessary to bypass that behaviour (most airports force you to reconnect after 30-45 minutes).
-
-<p align='center'>
-	<img src='screenshot2.png' width='50%'>
-</p>
+### Why do I need the `sudo`?
+To fix troublesome behaviour #3, the program needs to spoof the computer's `MAC address`. To do that, it needs `sudo` access. Thus, you will need to start this program as a root user, but once it's running, you do not need to input password any more.
--- index.js
+++ index.js
@@ -7,37 +7,74 @@ let failed = 0,
 const DEVICE = `$(networksetup -listallhardwareports | awk '$3=="Wi-Fi" {getline; print $2}')`,
   WIFI = `networksetup -setairportpower ${DEVICE}`,
   SPOOF_MAC =
+    // wifi needs to be turned on to change MAC address
+    `${WIFI} on && ` +
     // disconnect from any connected network first WITHOUT shutting off the Wi-Fi
-    '/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -z;' +
-    `ifconfig ${DEVICE} ether $(openssl rand -hex 6 | sed 's/\\(..\\)/\\1:/g; s/.$//')`,
+    '/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -z && ' +
+    `sudo ifconfig ${DEVICE} ether $(openssl rand -hex 6 | sed 's/\\(..\\)/\\1:/g; s/.$//')`,
   { lookup } = require('dns'),
   { execSync } = require('child_process'),
-  { exec } = require('sudo-prompt'),
+  { bgWhite, gray, underline } = require('chalk'),
   debug = require('debug'),
   info = debug('wifi:!'),
   error = debug('wifi:✗'),
   ok = debug('wifi:✓'),
+  msg = err =>
+    gray(
+      err
+        .toString()
+        .trim()
+        .split('\n')
+        .pop()
+    ),
   now = () => new Date().toLocaleString() + '\t',
   sleep = interval => setTimeout(check, interval * 1000),
   exponentiate = interval =>
     Math.min(interval * process.env.GROWTH_RATE, process.env.MAX_INTERVAL),
   restart = msg => {
-    execSync(`${WIFI} off; ${WIFI} on`)
+    try {
+      execSync(`${WIFI} off && ${WIFI} on`)
+    } catch (err) {
+      error(
+        `Network restart failed: ${msg(err)} ${bgWhite('(Is this running on a Mac?)')}`
+      )
+      process.exit(3)
+    }
+
     info(msg)
     sleep((errorInterval = exponentiate(errorInterval)))
   },
   check = () =>
-    lookup(process.env.TEST_WEBSITE, err => {
-      if (err && err.code == 'ENOTFOUND') {
+    lookup(process.env.TEST_SITE, err => {
+      if (err) {
+        if (err.code != 'ENOTFOUND') {
+          error(
+            `Lookup failed: ${msg(err)} ${bgWhite('(Is the test site set up correctly?)')}`
+          )
+          process.exit(2)
+        }
+
         error(now())
         okInterval = actualMin
 
         // fail too many times -> spoof MAC
-        if (++failed == process.env.FAIL_LIMIT)
-          exec(SPOOF_MAC, { name: 'Fix Shitty Wifi' }, () =>
-            restart('MAC address spoofed\t')
-          )
-        else restart('restarted network card\t')
+        if (++failed >= process.env.FAIL_LIMIT) {
+          let spoofErr
+
+          try {
+            execSync(SPOOF_MAC)
+          } catch (err) {
+            // this is the one error that is recoverable from, so don't exit
+            spoofErr = err
+            error(
+              `MAC address spoofing failed: ${msg(err)} ${bgWhite('(Is the Wi-Fi on?)')}`
+            )
+          }
+
+          if (spoofErr) {
+            sleep((errorInterval = exponentiate(errorInterval)))
+          } else restart('MAC address spoofed\t')
+        } else restart('Network restarted\t')
       } else {
         ok(now())
         errorInterval = actualMin
@@ -46,5 +83,10 @@ const DEVICE = `$(networksetup -listallhardwareports | awk '$3=="Wi-Fi" {getline
       }
     })
 
-info(`looking up ${process.env.TEST_WEBSITE}\t`)
+if (process.getuid()) {
+  error(`Startup failed. Please try again with ${underline('root')} privileges`)
+  process.exit(1)
+}
+
+info(`Looking up ${process.env.TEST_SITE}\t`)
 check()
